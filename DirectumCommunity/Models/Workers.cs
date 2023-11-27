69d594a6f87a4b6d293ffd24fbfbd317b6bbf656
Kira <KN17022004@yandex.ru>
2023-11-28 00:45:38 +0500

    Добавьте файлы проекта.

---
+++ DirectumCommunity/Models/Workers.cs
@@ -0,0 +1,18 @@
+﻿namespace DirectumCommunity.Data.Models
+{
+    public class Worker
+    {
+        public int id { get; set; }
+        public string? img { get; set; }
+        public string? name { get; set; }
+        public string? birthday { get; set; }
+        public string? organization { get; set; }
+        public string? city { get; set; }
+        public int mobilePhone { get; set; }
+        public int workPhone { get; set; }
+        public string? schedule { get; set; }
+        public string? email { get; set; }
+        public string? positionJob { get; set; }
+        public string? department { get; set; }
+    }
+}
